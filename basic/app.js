6d2d2fc2c12309a5114e34236018488522cf52a7
lzx <luanzhuxian@hotmail.com>
2020-06-08 15:02:20 +0800

    update

---
+++ basic/app.js
@@ -0,0 +1,26 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app)
+var fs = require('fs')
+
+app.listen(3000, () => {
+  console.log('listening on *:3000')
+})
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html', function (err, data) {
+    if (err) {
+      res.writeHead(500)
+      return res.end('Error loading index.html')
+    }
+
+    res.writeHead(200)
+    res.end(data)
+  })
+}
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' })
+  socket.on('test', function (data) {
+    console.log('from client', data)
+  })
+})
