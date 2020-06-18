401dcc5eadbb09c5800250b43a28646c46a60b70
lzx <luanzhuxian@hotmail.com>
2020-06-18 19:45:28 +0800

    update

--- chat/index.js
+++ chat/index.js
@@ -1,8 +1,10 @@
 const app = require('express')()
-const http = require('http').Server(app)
+const http = require('http')
 const Server  = require('socket.io')
-const io = Server(http)
 const crypto = require('crypto')
+
+const server = http.createServer(app)
+const io = Server(server)
 
 const users = new Map()
 const history = []
@@ -20,10 +22,10 @@ app.get('/', (req, res) => {
 io.use((socket, next) => {
     // const { uid } = socket.handshake.query
     const { id } = socket
-    console.log('id', id)
+    // console.log('id', id)
 
     if (id) {
-        users.set(id, { id })
+        users.set(id, socket)
         return next()
     }
     next(new Error('authentication error'))
@@ -33,36 +35,50 @@ function onConnect (socket) {
     
     const { on, id } = socket
 
-    socket.on = function (event) {
-        return new Promise((resolve, reject) => {
-            on.call(this, event, (result) => {
-                resolve(result)
-            })
-        })
-    }
+    // socket.on = function (event) {
+    //     return new Promise((resolve, reject) => {
+    //         on.call(this, event, (result) => {
+    //             resolve(result)
+    //         })
+    //     })
+    // }
 
-    socket.on('disconnect').then(() => {
+    socket.on('disconnect', () => {
         if (users.has(id)) {
             users.delete(id)
         }
         console.log(`user ${id} disconnected`)
-        // io.emit('left', id)
         socket.broadcast.emit('left', id)
     })
 
-    socket.on('message').then((msg) => {
+    socket.on('message', (msg) => {
         // if (uid !== id) return console.error('用户id不匹配')
 
         console.log(`receive message from ${id}: ${msg}`)
         history.push({ id, msg })
-        io.emit('message', { id, msg} )
+
+        const private = msg.match(/@([^ ]+) (.+)/)
+        if (private) {
+            const targetId = private[1]
+            const msg = private[2]
+            const targetSocket = users.get(targetId)
+            if (targetSocket) {
+                targetSocket.send({
+                    id,
+                    msg
+                })
+            }
+
+        } else {
+            io.emit('message', { id, msg} )
+        }
     })
 
-    socket.on('keypress').then(() => {
+    socket.on('keypress', () => {
         io.emit('keypress', id )
     })
 
-    socket.on('keypressdone').then(() => {
+    socket.on('keypressdone', () => {
         io.emit('keypressdone')
     })
 
@@ -77,6 +93,6 @@ function onConnect (socket) {
 
 io.on('connection', onConnect)
 
-http.listen(3000, () => {
+server.listen(3000, () => {
     console.log('listening on *:3000')
 })
